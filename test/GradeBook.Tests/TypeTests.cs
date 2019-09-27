b0f574f05280e148afc55185f713fdf9d5de718a
Ilias.Eleta <Ilias.Eleta@studentpartner.com>
2019-09-27 14:56:14 +0300

    commit staged

---
+++ test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class TypeTests
+    {
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            // arrange
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+
+        }
+
+            Book GetBook(string name)
+            {
+                return new Book(name);
+            }
+
+            // act
+          
+
+            // assert
+           
+        
+    }
+}
